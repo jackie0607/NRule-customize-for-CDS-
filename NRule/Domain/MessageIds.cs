d286014b09aca9fe591e02fd5e5891b636fcfbed
jackie0607 <jackie0607331008@gmail.com>
2018-02-05 14:49:02 +0800

    NRules for CDS v1.0 edit some file name

---
+++ NRule/Domain/MessageIds.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRule.Domain
+{
+    public class MessageIds
+    {
+        public string MessageName;
+        public string MessageId;
+        public List<AlarmRules> AlarmRuleslist;
+    }
+    public class AlarmRules
+    {
+        public string AlarmRuleName;
+        public string AlarmRuleId;
+        public List<RuleItem> RuleItemlist;
+    }
+    public class RuleItem
+    {       
+        public string AlarmRules;
+        public string ElmentName;
+        public int DataType;
+        public string RightStringValue = null;
+        public bool RightboolValue;
+        public double RightNumbericValue;
+        public string Operation;
+        public string Logic;
+        public bool result;
+    }
+}
